a0491c7aa9992d468aeb65d97addb63df991c847
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-08-09 21:09:44 +0200

    Uncomment read() method override in SX1262 class for implementation

--- components/wmbus_radio/transceiver_sx1262.h
+++ components/wmbus_radio/transceiver_sx1262.h
@@ -462,12 +462,11 @@ namespace wmbus_radio {
 class SX1262 : public RadioTransceiver {
 public:
   void setup() override;
-//  bool read(uint8_t *, size_t) override;
+  bool read(uint8_t *, size_t) override;
   void restart_rx() override;
   int8_t get_rssi() override;
   const char *get_name() override;
 protected:
-  bool read(uint8_t *, size_t) override;
   uint8_t offset;
 };
 } // namespace wmbus_radio
