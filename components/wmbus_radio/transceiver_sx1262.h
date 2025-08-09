c73e204328e8a31a2d96e706173c45afd3ced60f
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-08-09 20:02:31 +0200

    Change read() method return type from optional<uint8_t> to bool in SX1262 class

--- components/wmbus_radio/transceiver_sx1262.h
+++ components/wmbus_radio/transceiver_sx1262.h
@@ -462,7 +462,7 @@ namespace wmbus_radio {
 class SX1262 : public RadioTransceiver {
 public:
   void setup() override;
-  optional<uint8_t> read() override;
+  bool read() override;
   void restart_rx() override;
   int8_t get_rssi() override;
   const char *get_name() override;
