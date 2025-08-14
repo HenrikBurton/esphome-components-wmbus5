b782f172e7f5a641742ce84cc9703e6654594ae5
HenrikBurton <99271512+HenrikBurton@users.noreply.github.com>
2025-08-14 18:58:29 +0200

    Undo LOG level

--- components/wmbus_radio/component.cpp
+++ components/wmbus_radio/component.cpp
@@ -43,7 +43,7 @@ void Radio::loop() {
     return;
   }
 
-  ESP_LOGD(TAG, "Have data from radio (%zu bytes) [RSSI: %ddBm, mode: %s]",
+  ESP_LOGI(TAG, "Have data from radio (%zu bytes) [RSSI: %ddBm, mode: %s]",
            frame->data().size(), frame->rssi(), toString(frame->link_mode()));
 
   uint8_t packet_handled = 0;
